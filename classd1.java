6fe8519ed383bd47656c5591ff44a2f683c81138
bbyji07 <68051582+bbyji07@users.noreply.github.com>
2020-08-06 23:17:58 +0530

    creating classd1 java file to commit

---
+++ classd1.java
@@ -0,0 +1,6 @@
+public class classd1 {
+	public static void main(String[] args) {
+		//prints "Welcome message" in the terminal window.
+		System.out.println("Welcome to Jenkins tutorial using Java file");
+	}
+}
